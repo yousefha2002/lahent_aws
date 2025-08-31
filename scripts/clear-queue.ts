2a601ba950a9c3b8865b2f26abda4c6a11d6335e
yousefha2002 <yousefha2029@gmail.com>
2025-08-31 13:37:16 +0300

    clear queues

--- scripts/clear-queue.ts
+++ scripts/clear-queue.ts
@@ -1,24 +1,26 @@
-import Queue from 'bull';
+// scripts/clear-queue.ts
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-async function clearQueue() {
-    try {
-        const queue = new Queue('orders', {
-        redis: {
-            host: process.env.REDIS_HOST,
-            port: Number(process.env.REDIS_PORT),
-            tls: {}, // ضروري لو AWS MemoryDB
-        },
-        });
+import Bull from 'bull'; // استيراد صحيح لإصدار Bull الحالي
 
-        await queue.obliterate({ force: true });
-        console.log('Queue cleared successfully!');
-    } catch (err) {
-        console.error('Failed to clear queue:', err);
-    } finally {
-        process.exit(0);
-    }
+async function clearQueue() {
+  try {
+    const queue = new Bull('orders', {
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        tls: {}, // ضروري لو AWS MemoryDB
+      },
+    });
+
+    await queue.obliterate({ force: true });
+    console.log('Queue cleared successfully!');
+  } catch (err) {
+    console.error('Failed to clear queue:', err);
+  } finally {
+    process.exit(0);
+  }
 }
 
 clearQueue();
