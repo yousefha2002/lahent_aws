9c9008892befe854fd01f77498ebbcbf3d252394
yousefha2002 <yousefha2029@gmail.com>
2025-08-31 09:44:30 +0300

    edit accept langauge

--- src/main.ts
+++ src/main.ts
@@ -35,10 +35,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json()); 
-  app.use((req, res, next) => {
-    console.log('Headers:', req.headers);
-    next();
-  });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
