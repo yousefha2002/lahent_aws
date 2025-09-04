1e104da3c560f25f3f49d49855dc93a616b8daae
yousefha2002 <yousefha2029@gmail.com>
2025-09-04 12:09:58 +0300

    active of product

--- src/modules/product/dto/full-product-with-details.dto.ts
+++ src/modules/product/dto/full-product-with-details.dto.ts
@@ -40,4 +40,8 @@ export class fullProductDetailsWihtPrivateDetails extends FullProductDetailsDto 
     @Expose()
     @ApiProperty({ example: 250 })
     sales: number;
+
+    @Expose()
+    @ApiProperty({ example: true })
+    isActive: boolean;
 }
