a4300e2425d532db3b20f3d72d9e23401f0481fe
yousefha2002 <yousefha2029@gmail.com>
2025-09-15 13:05:12 +0300

    return pickedup methods

--- src/modules/store/dto/full-details-store.dto.ts
+++ src/modules/store/dto/full-details-store.dto.ts
@@ -2,11 +2,21 @@ import { Expose, Type } from 'class-transformer';
 import { StoreDto } from './Store.dto';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class FullDetailsStoreDto {
-    @ApiProperty({ type: [StoreDto] })
+export class StoreWihtPickedUpMethods extends StoreDto {
+    @ApiProperty({example: true})
     @Expose()
-    @Type(() => StoreDto)
-    store: StoreDto;
+    inSotre:boolean
+
+    @ApiProperty({ example: true })
+    @Expose()
+    driveThru: boolean;
+}
+
+export class FullDetailsStoreDto {
+    @ApiProperty({ type: [StoreWihtPickedUpMethods] })
+    @Expose()
+    @Type(() => StoreWihtPickedUpMethods)
+    store: StoreWihtPickedUpMethods;
 
     @ApiProperty({description: 'Indicates if the store is marked as favorite by the current customer',example: true})
     @Expose()
