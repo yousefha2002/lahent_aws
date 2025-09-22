4bd0522716cb7bcc7f5b3e94df4619532b936a92
yousefha2002 <yousefha2029@gmail.com>
2025-09-22 12:51:23 +0300

    add provider in payment module

--- src/modules/payment_card/payment_card.module.ts
+++ src/modules/payment_card/payment_card.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentCardService } from './payment_card.service';
 import { PaymentCardController } from './payment_card.controller';
+import { PaymentCardProvider } from './providers/payment_card.provider';
 
 @Module({
   controllers: [PaymentCardController],
-  providers: [PaymentCardService],
+  providers: [PaymentCardService,...PaymentCardProvider],
 })
 export class PaymentCardModule {}
