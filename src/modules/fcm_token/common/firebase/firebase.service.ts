85df3e6b6179164969f7f0205ca3c9bf9584a996
yousefha2002 <yousefha2029@gmail.com>
2025-09-24 13:52:58 +0300

    remove json file

--- src/modules/fcm_token/common/firebase/firebase.service.ts
+++ src/modules/fcm_token/common/firebase/firebase.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { join } from 'path';
-const serviceAccount = require(join(__dirname, 'lahentnotification-firebase-adminsdk-fbsvc-eaea030a60.json'));
+// const serviceAccount = require(join(__dirname, 'lahentnotification-firebase-adminsdk-fbsvc-eaea030a60.json'));
 
 @Injectable()
 export class FirebaseService {
     constructor() {
-        admin.initializeApp({credential: admin.credential.cert(serviceAccount as any)});
+        // admin.initializeApp({credential: admin.credential.cert(serviceAccount as any)});
     }
 
     async sendNotificationToMultiple(tokens: string[], title: string, body: string, data?: Record<string, string>) 
