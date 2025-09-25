91c983f6cd0e9860aaf087af5e26edd4e92f1b6c
yousefha2002 <yousefha2029@gmail.com>
2025-09-25 12:55:14 +0300

    add response dto for validation

--- src/modules/payment_session/payment_session.controller.ts
+++ src/modules/payment_session/payment_session.controller.ts
@@ -2,14 +2,17 @@ import {Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { PaymentSessionService } from './payment_session.service';
 import { ApiOperation, ApiParam, ApiResponse, ApiSecurity } from '@nestjs/swagger';
 import { CustomerGuard } from 'src/common/guards/customer.guard';
+import { Serilaize } from 'src/common/interceptors/serialize.interceptor';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Controller('payment-session')
 export class PaymentSessionController {
   constructor(private readonly paymentSessionService: PaymentSessionService) {}
 
+  @Serilaize(PaymentSessionDto)
   @ApiOperation({ summary: 'Check payment status by paymentId (customer only)' })
   @ApiParam({ name: 'paymentId', description: 'Payment ID', example: 1 })
-  @ApiResponse({ status: 200, description: 'Payment status returned successfully' })
+  @ApiResponse({ status: 200, type:PaymentSessionDto })
   @ApiResponse({ status: 404, description: 'Payment session not found' })
   @ApiSecurity('access-token')
   @UseGuards(CustomerGuard)
