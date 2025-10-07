88a678abfc71ea2a0feec570edbc5ebaee54253f
yousefha2002 <yousefha2029@gmail.com>
2025-10-07 10:30:38 +0300

    include owner when get store for admin

--- src/modules/store/dto/responses/admin-store.dto.ts
+++ src/modules/store/dto/responses/admin-store.dto.ts
@@ -2,10 +2,16 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Expose, Type } from "class-transformer";
 import { storeForAction } from "./store-for-action.dto";
 import { CommissionPercentDto } from "src/modules/store_commission/dto/store_comimssion.dto";
+import { OwnerDto } from "src/modules/owner/dto/owner.dto";
 
 export class StoreAdminViewDto extends storeForAction {
     @ApiProperty({ type: CommissionPercentDto })
     @Expose()
     @Type(() => CommissionPercentDto)
     commission: CommissionPercentDto;
+
+    @ApiProperty({ type: OwnerDto })
+    @Type(() => OwnerDto)
+    @Expose()
+    owner:OwnerDto
 }
