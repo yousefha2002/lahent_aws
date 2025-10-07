824cefe2ff8583426be8d9aba4c99a363703af59
yousefha2002 <yousefha2029@gmail.com>
2025-10-07 11:25:25 +0300

    remove isString from dto of gift template action

--- src/modules/gift_template/dto/update-gift-template.dto.ts
+++ src/modules/gift_template/dto/update-gift-template.dto.ts
@@ -9,11 +9,9 @@ export class UpdateGiftTemplateDto {
 
     @ApiProperty({ example: "2025-12-31T23:59:59.000Z", required: false, nullable: true })
     @IsOptional()
-    @IsString()
     startDate?: string;
 
     @ApiProperty({ example: "2025-12-31T23:59:59.000Z", required: false, nullable: true })
     @IsOptional()
-    @IsString()
     endDate?: string;
 }
